39cebd6e1e409f344e6f4c5e975b51fa76837fe7
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-08-21 22:39:19 +0200

    Add domready and move script tag into head

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var byTag = require('by/tag');
 var style = require('dom-style');
 var newElement = require('new-element');
 var append = require('insert/append');
+var domready = require('domready');
 require('./style.css');
 
-new Vivus('mu', { type: 'oneByOne', duration: 200 }, function () {
-  style(byTag('path')[0], 'fill', 'white');
-  style(document.body, 'background-color', 'black');
-  var div = newElement('<div>{text}</div>', { text: '404' });
-  append(document.body, div);
+domready(function () {
+  new Vivus('mu', { type: 'oneByOne', duration: 200 }, function () {
+    style(byTag('path')[0], 'fill', 'white');
+    style(document.body, 'background-color', 'black');
+    var div = newElement('<div>{text}</div>', { text: '404' });
+    append(document.body, div);
+  });
 });
