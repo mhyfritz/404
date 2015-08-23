2f6dba103b59d42ace967f65bb6f6c8bcc20fa2c
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-08-23 15:12:05 +0200

    Use query selector

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Vivus = require('vivus');
-var byTag = require('by/tag');
+var query = require('by/query');
 var style = require('dom-style');
 var newElement = require('new-element');
 var append = require('insert/append');
@@ -8,7 +8,7 @@ require('./style.css');
 
 domready(function () {
   new Vivus('mu', { type: 'oneByOne', duration: 200 }, function () {
-    style(byTag('path')[0], 'fill', 'white');
+    style(query('path'), 'fill', 'white');
     style(document.body, 'background-color', 'black');
     var div = newElement('<div>{text}</div>', { text: '404' });
     append(document.body, div);
