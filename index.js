8b85e5542a2b906c493037d45025d497482f3fd9
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-11-10 22:22:23 +0100

    Vertical centering

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var Vivus = require('vivus');
 var query = require('by/query');
 var style = require('dom-style');
-var newElement = require('new-element');
-var append = require('insert/append');
 var domready = require('domready');
+
 require('./style.css');
 
 domready(function () {
   new Vivus('mu', { type: 'oneByOne', duration: 200 }, function () {
     style(query('path'), 'fill', 'white');
     style(document.body, 'background-color', 'black');
-    var div = newElement('<div>{text}</div>', { text: '404' });
-    append(document.body, div);
   });
 });
